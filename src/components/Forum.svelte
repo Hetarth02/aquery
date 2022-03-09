aeb8749d8884f5e3a941112df6b8ef8867ddbd70
Hetarth Shah <80420115+Hetarth02@users.noreply.github.com>
2022-03-09 13:21:36 +0530

    Minor updates

--- src/components/Forum.svelte
+++ src/components/Forum.svelte
@@ -1,7 +1,5 @@
 <script>
   import { link } from "svelte-spa-router";
-  import NewButton from "../components/NewButton.svelte";
-  import SortButton from "../components/SortButton.svelte";
 
   export let forum_data = undefined;
 </script>
