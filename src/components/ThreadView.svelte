991c1dc9d3b654fb40dd385b2160723b51e3fc91
Hetarth Shah <80420115+Hetarth02@users.noreply.github.com>
2022-03-10 18:57:05 +0530

    Minor Updates in ThreadView

--- src/components/ThreadView.svelte
+++ src/components/ThreadView.svelte
@@ -1,4 +1,17 @@
 <script>
   import Nav from "../components/Nav.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
 </script>
+
+<style>
+  p {
+    color: black;
+  }
+</style>
+
 <Nav />
+<div>
+  <Sidebar>
+    <p>Sidebar Content</p>
+  </Sidebar>
+</div>
