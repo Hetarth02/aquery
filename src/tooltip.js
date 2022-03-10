a8d780dd8fdaf8e09ac4910a547b6a3e9fcd15d4
Hetarth Shah <80420115+Hetarth02@users.noreply.github.com>
2022-03-10 15:52:57 +0530

    minor comment change

--- src/tooltip.js
+++ src/tooltip.js
@@ -1,5 +1,5 @@
 import tippy from "tippy.js";
-import "tippy.js/dist/tippy.css"; // Link tippy.css explicitly in index.html
+import "tippy.js/dist/tippy.css"; // Link tippy.css explicitly in index.html and remove this import from here
 export default function tooltip(
   node,
   params = {
